692d6d1bbdd1a39b59d043262044472abcba0c8b
s0raya <soraya.alcobet@gmail.com>
2024-02-16 23:33:02 +0100

    urlencoded

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express()
 const PORT = 3000;
 const { dbConnection } = require('./config/config.js');
-const routes = require('./routes/posts.js')
+const routes = require('./routes/posts.js');
+const { urlencoded } = require('body-parser');
 
 app.disable('x-powered-by');
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', routes);
 
--- test/post.test.js
+++ test/post.test.js
@@ -10,9 +10,12 @@ describe("post", () => {
     test("create post", async() => {
         let postsCount = await Post.countDocuments({});
         expect(postsCount).toBe(0);
-        resPost = await request(app).post('/create').send(post).expect(201);
+        resPost = (await request(app).post('/create')).send(post).expect(201);
         postsCount = await Post.countDocuments({});
         expect(postsCount).toBe(1);
+        expect(resPost.body._id).toBeDefined();
+        expect(resPost.body.createdAt).toBeDefined();
+        expect(resPost.body.updatedAt).toBeDefined();
     });
     afterAll(() => {
         return Post.deleteMany();
